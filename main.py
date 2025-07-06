debe9a8af558ecae9613816736d12b55ae1b3955
DauntlessK <133259396+DauntlessK@users.noreply.github.com>
2025-07-06 17:44:19 -0400

    Maze mostly complete, work on nodes and starting queue / heap

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import heapq
+
 class Maze():
 
     def __init__(self):
-        #Maze stored as a 2D array, where 0 = empty box, 1 = start, 2 = finish, 8 = blank space
+        # Maze stored as a 2D array, where 0 = empty box, 1 = start, 2 = finish, 8 = blank space
         self.layout = [[0,1,0,8,0,0],
                        [0,8,8,8,8,0],
                        [0,8,0,2,8,0],
@@ -9,12 +11,15 @@ class Maze():
                        [0,0,0,0,0,0]
                     ]
         
-        #Wind Parameters
-        windDirection = 0               # 0 = West, 1 = North, 2 = East, 3 = South
-        MOVEMENT_COST_WITH_WIND = 1     # cost in spaces to move in the direction of the wind
-        MOVEMENT_COST_PERP_WIND = 2     # cost in spaces to move perpendicular to the wind
-        MOVEMENT_COST_AGAINST_WIND = 3  # cost in spaces to move against the direction of the wind
+        # Wind Parameters
+        self.windDirection = 0               # 0 = West, 1 = North, 2 = East, 3 = South
+        self.MOVEMENT_COST_WITH_WIND = 1     # cost in spaces to move in the direction of the wind
+        self.MOVEMENT_COST_PERP_WIND = 2     # cost in spaces to move perpendicular to the wind
+        self.MOVEMENT_COST_AGAINST_WIND = 3  # cost in spaces to move against the direction of the wind
 
+        # Maze information
+        self.numOfRows = len(self.layout)
+        self.numOfColumns = len(self.layout[0])
         self.startBox = {}
         self.finishBox = {}
         self.setBoxes()
@@ -56,14 +61,65 @@ class Maze():
         return self.startBox
 
     # Returns dictionary object containing row and column of box
-    def getEndBox(self):
+    def getFinishBox(self):
         return self.finishBox
-            
+
+    # Returns wind direction in int format (0 is West)
+    def getWindDirection(self):
+        return self.windDirection
+    
+    # Figures out the cost of a movement in a given direction based on wind
+    # Returns int of # of movement cost
+    def getMovementCost(self, direction):
+        if direction == self.windDirection:
+            return self.MOVEMENT_COST_WITH_WIND
+        elif (direction - 2 == self.windDirection) or (direction + 2 == self.windDirection):
+            return self.MOVEMENT_COST_AGAINST_WIND
+        else:
+            return self.MOVEMENT_COST_PERP_WIND
+        
+    # Returns the manhattan distance of a given box at row, col to the maze finish box
+    def getManhattanDistance(self, row, col):
+        manDistRow = abs(row - self.finishBox["Row"])
+        manDisCol = abs(col - self.finishBox["Column"])
+        return manDistRow + manDisCol
+        
+    # Returns True if there's a box that can be explored, otherwise returns False
+    def boxExists(self, row, col):
+        if row > self.numOfRows or col > self.numOfColumns:
+            return False
+        elif row < 0 or col < 0:
+            return False
+        box = self.getBoxType(row, col)
+        if box == "Empty Box":
+            return False
+        else:
+            return True
+        
+# One individual node that holds a box's info when it is discovered, then added to the queue
+class boxNode():
+    
+    def __init__(self, maze, num, movementCost, row, col):
+        self.num = num                              # discovery number
+        self.movementCost = movementCost            # total movement cost of this box along with every square to date
+        self.maze = maze
+        self.row = row
+        self.column = col
+        self.manhattanDistance = self.maze.getManhattanDistance(self.row, self.column)
+        self.totalCost = self.movementCost + self.manhattanDistance
+        self.next = None
+
+# Responsible for holding the frontier as a sorted list of box nodes
 class Queue():
 
     def __init__(self):
+        self.head = None
+
+    # Inserts a new node into the linked list, keeping it sorted according to totalCost (tiebreak goes to num)
+    def insert(newNode):
         pass
 
 m = Maze()
 print(m.getBoxType(0,2))
 print(m.finishBox["Row"])
+print(m.getMovementCost(2))
